2d04fc11df394119f12c6eb8b5dc00b144e413f7
João Francisco <jo.santosp77@gmail.com>
2021-07-19 05:08:46 -0300

    Update ApiKey.kt
    Removed api key

--- app/src/main/java/com/example/newsfeed/data/api/ApiKey.kt
+++ app/src/main/java/com/example/newsfeed/data/api/ApiKey.kt
@@ -2,6 +2,6 @@ package com.example.newsfeed.data.api
 
 class ApiKey {
     companion object {
-        const val API_KEY = "425f95b425a846d18b65bee632358dd6"
+        const val API_KEY = "INSERT API KEY"
     }
 }
