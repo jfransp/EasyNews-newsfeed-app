8b7559bad33c27308bbbe3de08391b617cb543fe
jfransp <jo.santosp77@gmail.com>
2021-07-28 17:53:47 -0300

    Added a comment to API key file

--- app/src/main/java/com/example/newsfeed/data/api/ApiKey.kt
+++ app/src/main/java/com/example/newsfeed/data/api/ApiKey.kt
@@ -3,5 +3,6 @@ package com.example.newsfeed.data.api
 class ApiKey {
     companion object {
         const val API_KEY = "INSERT API KEY"
+        // Get your API key at https://newsapi.org/
     }
 }
